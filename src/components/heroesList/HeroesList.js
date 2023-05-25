2eeb16af1734984ac8cc942cc912c7d6688badd9
Evgeny <example@gmail.com>
2023-05-25 11:39:13 +0300

    first commit

---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,58 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useHttp} from '../../hooks/http.hook';
+import { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { heroDeleted, fetchHeroes, filteredHeroesSelector } from './heroesSlice';
+
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+
+
+const HeroesList = () => {
+    const filteredHeroes = useSelector(filteredHeroesSelector);
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(fetchHeroes());
+        // eslint-disable-next-line
+    }, []);
+
+    const onDelete = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(data => console.log(data, 'Deleted'))
+            .then(dispatch(heroDeleted(id)))
+            .catch(err => console.log(err));
+        // eslint-disable-next-line  
+    }, [request]);
+
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderHeroesList = (arr) => {
+        
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Героев пока нет</h5>
+        }
+
+        return arr.map(({id,...props}) => {
+            return <HeroesListItem key={id} onDelete={() => onDelete(id)} {...props}/>
+        })
+    }
+    const elements = renderHeroesList(filteredHeroes);
+    return (
+        <ul>
+            {elements}
+        </ul>
+    )
+}
+
+export default HeroesList;
+
+
+
